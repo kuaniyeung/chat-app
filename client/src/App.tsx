c76216f777d8cea43320be7b7d1ae642d12a177a
kuaniyeung@gmail.com <kuaniyeung@gmail.com>
2023-12-18 19:16:41 -0500

    removed testing code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,56 +13,6 @@ function App() {
   //   "Change Enable Email's Comfirm Email settings back to enabled when project is finalized"
   // );
 
-  // ------ TESTING ------ //
-
-  // const testUser = {
-  //   id: "c2025409-fd94-4211-9a54-c39d6d45e1f8",
-  //   email: "test@test.com",
-  //   display_name: "Mememe",
-  // };
-
-  // const verifiedContact = {
-  //   id: "060a8a80-df76-44a1-a5b5-e3b29f65a87b",
-  //   display_name: "Contact1",
-  // };
-
-  // const payload = {
-  //   name: "Testing room",
-  //   members: [
-  //     { id: "060a8a80-df76-44a1-a5b5-e3b29f65a87b", display_name: "Contact1" },
-  //     { id: "f57aa67f-3c2e-4616-a74a-73e3030e9367", display_name: "TestAgain" },
-  //   ],
-  // };
-
-  // const testing = async () => {
-  //   try {
-  //     const { data: contactsData, error } = await supabase
-  //       .from("contacts")
-  //       .select(
-  //         "contact_id, contacts_contact_id_fkey!inner(display_name)"
-  //       )
-  //       .eq("user_id", testUser.id);
-
-  //     if (error) throw error;
-
-  //     // console.log(contactsData);
-  //     const formattedContacts = contactsData.map((contact) => {
-  //       return {
-  //         id: contact.contact_id,
-  //         display_name: contact.contacts_contact_id_fkey.display_name,
-  //       };
-  //     });
-
-  //     console.log(formattedContacts);
-  //   } catch (error) {
-  //     return console.error(error);
-  //   }
-  // };
-
-  // testing();
-
-  // console.log(contacts);
-
   return (
     <Routes>
       <Route
